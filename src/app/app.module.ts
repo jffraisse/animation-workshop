7d5fe94c6618c48e4e960d50dacd31d3ce636b7c
Jean-François Fraisse <fraisse.jean.francois@gmail.com>
2016-10-24 16:12:29 +0200

    add character component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CharacterComponent } from './character/character.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterComponent
   ],
   imports: [
     BrowserModule,
