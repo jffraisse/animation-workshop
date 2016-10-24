0a5539c332ff808bc17203ddb22b64299771e704
Jean-François Fraisse <fraisse.jean.francois@gmail.com>
2016-10-24 16:12:29 +0200

    add character component

---
+++ src/app/character/character.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { CharacterComponent } from './character.component';
+
+describe('Component: Character', () => {
+  it('should create an instance', () => {
+    let component = new CharacterComponent();
+    expect(component).toBeTruthy();
+  });
+});
